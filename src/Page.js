bce8c8bf81a0abc5851ae7dcf3d8910a994fd9a6
newit-tindm <tindm@newit.co.jp>
2021-08-05 12:22:36 +0700

    update

--- src/Page.js
+++ src/Page.js
@@ -48,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: '10px',
     marginRight: '10px',
   },
-  progress: {
+  progressStyle: {
     width: '50%',
     marginLeft: '25%',
     marginBottom: '20px',
@@ -71,6 +71,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function LinearProgressWithLabel(props) {
+  console.log('progress ', props.value);
   return (
     <Box display="flex" alignItems="center">
       <Box width="100%" mr={1}>
@@ -94,19 +95,14 @@ LinearProgressWithLabel.propTypes = {
 
 export default function Page() {
   const classes = useStyles();
-  const [progress, setProgress] = useState(0);
 
-  const url_blocked_ids = 'https://d32kd3i4w6exck.cloudfront.net/api/get-blocked-ids';
+  const [itemsNotBlocked, setItemsNotBlocked] = useState([]);
 
-  const [itemNotBlocked, setItemNotBlocked] = useState([]);
-
-  const [itemShouldBeDeleted, setItemShouldBeDeleted] = useState([]);
+  const [itemsShouldBeDeleted, setItemsShouldBeDeleted] = useState([]);
 
   const [uploadIds, setUploadIds] = useState([]);
 
   const [blockedIds, setBlockedIds] = useState([]);
-
-  const [status, setStatus] = useState();
 
   const [concurrency, setConcurrency] = useState('');
 
@@ -114,98 +110,95 @@ export default function Page() {
 
   const [isLoading, setIsLoading] = useState(false);
 
+  const [countItems, setCountItems] = useState(0);
+
+  const [progress, setProgress] = useState(0);
+
+  const url_blocked_ids = 'https://d32kd3i4w6exck.cloudfront.net/api/get-blocked-ids';
+
   const url_sold_out = 'https://d32kd3i4w6exck.cloudfront.net/api/is-sold-out/33776097';
 
   async function getBlockedIds() {
-    setIsLoading(true);
+    if(!blockedIds.length) {
+      setIsLoading(true);
+    }
+    
     // get blocked ids API
     let blocked_ids = await APIModuleData.getAPI(url_blocked_ids).then(res => res);
-    setBlockedIds(blocked_ids);
 
+    if(uploadIds.length) {
+      const remove_empty_line = removeEmptyLines(uploadIds);
+      setUploadIds(dataOutput(remove_empty_line));
+      const items_not_blocked = getItemsNotBlocked(blocked_ids, remove_empty_line);
+      setCountItems(items_not_blocked.length);
+      setItemsNotBlocked(dataOutput(items_not_blocked));
+    }
+
+    setBlockedIds(blocked_ids);
   }
 
-  useEffect(() => {
-    console.log('object');
+  const removeEmptyLines = (data) => {
+    const convert_to_arr = convertStringToArr(data);
+    return convert_to_arr.filter(item => item);
+  }
 
-    if(blockedIds.length > 0) {
-      setUploadIds([]);
-    }
-  },[uploadIds])
-
-  function convertStringToArr(stringArr) {
-    let arr = [];
+  function convertStringToArr(str) {
     let re = /\s|,\s|,/; // ' ' or ', ' or '\n' 
-    arr = stringArr.split(re);
-    
+    let arr = str.split(re);
     return arr.map(item => {
       return parseInt(item)
     });
   }
 
   function getItemsNotBlocked(blocked_ids, upload_ids) {
-      let items_not_blocked = upload_ids.filter(item => {
-        // upload ids - blocked ids
-        if(!blocked_ids.includes(item)) {
-          return item;
-        }
-      })
+    let items_not_blocked = upload_ids.filter(item => {
+      // upload ids - blocked ids
+      if(!blocked_ids.includes(item)) {
+        return item;
+      }
+    })
 
-      return dataOutput(items_not_blocked);
+    return items_not_blocked;
   }
-
-  const urls = [
-    'https://jsonplaceholder.typicode.com/todos/1',
-    'https://jsonplaceholder.typicode.com/todos/2',
-    'https://jsonplaceholder.typicode.com/todos/3',
-    'https://jsonplaceholder.typicode.com/todos/4',
-    'https://jsonplaceholder.typicode.com/todos/5',
-    'https://jsonplaceholder.typicode.com/todos/6',
-    'https://jsonplaceholder.typicode.com/todos/7',
-    'https://jsonplaceholder.typicode.com/todos/8',
-    'https://jsonplaceholder.typicode.com/todos/9',
-    'https://jsonplaceholder.typicode.com/todos/10'
-  ];
   
-  async function getItemShouldBeDeleted() {
-    const queue = new PQueue({ concurrency: concurrency });
+  async function getItemsShouldBeDeleted() {
+    let sold_out = await APIModuleData.getAPI(url_sold_out).then(res => res.status);
 
-    const items = [];
+    if (true && itemsNotBlocked.length) {
+      const items_should_be_deleted = convertStringToArr(itemsNotBlocked);
+      
+      const queue = new PQueue({ concurrency: concurrency });
 
-    let count = 0;
+      const items = [];
 
-    // Emitted every time the add method is called and the number of pending or queued tasks is increased.
-    queue.on('add', () => {
-      console.log(`Task is added.  Size: ${queue.size}  Pending: ${queue.pending}`);
-    });
-    
-    // Emitted every time a task is completed and the number of pending or queued tasks is decreased. 
-    queue.on('next', () => {
-      console.log(`Task is completed.  Size: ${queue.size}  Pending: ${queue.pending}`);
-    });
+      let count = 0;
 
-    // Emitted as each item is processed in the queue for the purpose of tracking progress.
-    queue.on('active', () => {
-      count++;
-      // console.log(`Working on item #${++count}.  Size: ${queue.size}  Pending: ${queue.pending}`);
-      let percent = count*100/urls.length;
-      setProgress(percent);
-    });
+      // Emitted every time the add method is called and the number of pending or queued tasks is increased.
+      queue.on('add', () => {
+        console.log(`Task is added.  Size: ${queue.size}  Pending: ${queue.pending}`);
+      });
+      
+      // Emitted every time a task is completed and the number of pending or queued tasks is decreased. 
+      queue.on('next', () => {
+        console.log(`Task is completed.  Size: ${queue.size}  Pending: ${queue.pending}`);
+        let percent = (++count)*100/items_should_be_deleted.length;
+        setProgress(percent);
+        if(percent === 100) {
+          alert('Done!');
+        }
+      });
 
-    // all promise completed and queue empty
-    queue.on('idle', () => {
-      console.log(`Queue is idle.  Size: ${queue.size}  Pending: ${queue.pending}`);
-      setItemShouldBeDeleted(dataOutput(items));
-      alert('Done!')
-    });
-    
-    urls.map(url => {
-      queue.add(() => APIModuleData.getAPI(url).then(res => items.push(res.id)))
-    });
-
-    // api sold out
-    await APIModuleData.getAPI(url_sold_out).then(res => {
-      setStatus(res.status)
-    });
+      // all promise completed and queue empty
+      queue.on('idle', () => {
+        console.log(`Queue is idle.  Size: ${queue.size}  Pending: ${queue.pending}`);
+        setItemsShouldBeDeleted(dataOutput(items));
+        
+      });
+      
+      items_should_be_deleted.map(item => {
+        queue.add(() => items.push(item));
+      });
+    }
   }
 
   function handleConcurrency(e) {
@@ -222,13 +215,16 @@ export default function Page() {
     let convert_blocked_ids = await convertStringToArr(blocked_ids);
     let convert_upload_ids = await convertStringToArr(upload_ids);
     let items_not_blocked = await getItemsNotBlocked(convert_blocked_ids, convert_upload_ids);
-    return setItemNotBlocked(items_not_blocked);
+    setCountItems(items_not_blocked.length);
+    return setItemsNotBlocked(dataOutput(items_not_blocked));
   }
 
   const handleUploadIds = (e) => {
+    setUploadIds(e.target.value)
     if (!blockedIds.length) { // blocked ids is null
-      // setUploadIds()
-      setItemNotBlocked(e.target.value)
+      setItemsNotBlocked(e.target.value);
+      let items = convertStringToArr(e.target.value);
+      setCountItems(items.length);
     } else {
       handleItemsNotBlocked(blockedIds, e.target.value);
     }
@@ -248,7 +244,7 @@ export default function Page() {
             maxRows={30} 
             minRows={30}
             onChange={handleUploadIds}
-            defaultValue={uploadIds}
+            value={uploadIds}
             placeholder="
               35001159
               35025484
@@ -273,29 +269,34 @@ export default function Page() {
         </Grid>
         <Grid item xs={3} md={3} className={classes.display}>
           <label className={classes.button}>Items not blocked</label>
-          <TextareaAutosize aria-label="maximum height" maxRows={30} minRows={30} defaultValue={itemNotBlocked} />
+          <TextareaAutosize aria-label="maximum height" maxRows={30} minRows={30} defaultValue={itemsNotBlocked} />
         </Grid>
         <Grid item xs={3} md={3} className={classes.display}>
           <label className={classes.button}>Item should be deleted</label>
-          <TextareaAutosize aria-label="maximum height" maxRows={30} minRows={30} defaultValue={status ? itemShouldBeDeleted : ''} />
+          <TextareaAutosize aria-label="maximum height" maxRows={30} minRows={30} defaultValue={itemsShouldBeDeleted} />
         </Grid>
       </Grid>
       <Grid container spacing={2}>
-        <div className={classes.progress}>
+        <div className={classes.progressStyle}>
           <LinearProgressWithLabel value={progress} />
         </div>
       </Grid>
       <Grid container spacing={2} className={classes.concurrency}>
-        <label >Concurrency</label>
-        <TextField 
-          id="outlined-basic" 
-          size="small" 
-          type="number"
-          variant="outlined" 
-          className={classes.btnConcurrency} 
-          onChange={handleConcurrency} 
-        />
-        <Button variant="contained" onClick={getItemShouldBeDeleted} disabled={disabled}>Fetch</Button>
+        <Box component="div" m={1} >
+          <label >Concurrency</label>
+          <TextField 
+            id="outlined-basic" 
+            size="small" 
+            type="number"
+            variant="outlined" 
+            className={classes.btnConcurrency} 
+            onChange={handleConcurrency} 
+          /> 
+          <Button variant="contained" onClick={getItemsShouldBeDeleted} disabled={disabled}>Fetch</Button>
+        </Box>
+        <Box component="div" mr={15} m={3} >
+          <label >Items: {countItems} </label>
+        </Box>
       </Grid>
     </div>
   );
