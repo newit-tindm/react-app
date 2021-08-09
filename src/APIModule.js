fd3f1a6a0c92bef2c13ba8810ba325f6a93e1f95
newit-tindm <tindm@newit.co.jp>
2021-08-09 09:13:01 +0700

    catch error api

--- src/APIModule.js
+++ src/APIModule.js
@@ -8,9 +8,24 @@ class APIModule {
     }
 
     async getAPISoldOut(url, id) {
-        const res = await axios.get(url);
-        const item = res.data;
-        return [item, id];
+        try {
+            const res = await axios.get(url);
+            const item = res.data;
+            return [item, id];
+        } catch (error) {
+            if (error.response) {
+                // Request made and server responded
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+        }
     }
 }
 
